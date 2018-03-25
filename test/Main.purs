717fa6900b1a4a1679adff809926df499154ffb8
Gabe Johnson <gijohnson105@gmail.com>
2018-03-24 23:52:53 -0500

    Type checked first JS expression

--- test/Main.purs
+++ test/Main.purs
@@ -2,11 +2,16 @@ module Test.Main where
 
 import Prelude
 
+import Babylon.Types as B
 import Clean (runTypeInference, typeInference)
-import Clean.Expressions (Exp(..), Lit(..))
+import Clean.Expressions (Exp(..), Lit(..), babylonToClean)
+import Control.Comonad (extract)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Except (Except, runExceptT, withExceptT)
 import Data.Either (Either(..))
+import Data.Foldable (foldr)
+import Data.Foreign (F)
 import Data.Map as Map
 import Data.Traversable (traverse_)
 import Data.Tuple (Tuple(..))
@@ -36,13 +41,33 @@ e5 = EAbs "m" (ELet "y" (EVar "m")
                (ELet "x" (EApp (EVar "y") (ELit (LBoolean true)))
                 (EVar "x")))
 
-test :: forall e. Exp -> Eff (console :: CONSOLE | e) Unit
-test e = do
-  Tuple res _ <- runTypeInference (typeInference Map.empty e)
-  case res of
+jsToClean :: forall e. String -> Except String Exp
+jsToClean js = do
+  ast <- relaxF $ B.parseExpression' js
+  babylonToClean ast
+
+logResults :: forall e a. Show a => Exp -> (Either String a) -> Eff (console :: CONSOLE | e) Unit
+logResults e r = do
+  case r of
     Left err -> log $ "error: " <> err
     Right t  -> log $ show e <> " :: " <> show t
 
+
+test :: forall e. Exp -> Eff (console :: CONSOLE | e) Unit
+test e = do
+  Tuple r _ <- runTypeInference (typeInference Map.empty e)
+  logResults e r
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
+  -- Test a Clean expression
   traverse_ test [e0, e1, e2, e3, e4, e5]
+
+  -- Test JS
+  let res = extract $ runExceptT $ jsToClean "(x => x)('hello types!')"
+  case res of
+    Left err -> log $ "JS error: " <> err
+    Right exp -> test exp
+
+relaxF :: F ~> Except String
+relaxF = withExceptT $ foldr append "" <<< (show <$> _)
